6f215f15099a0f9c83735181df26f952f61886d8
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-17 16:50:42 +0300

    Completed lab

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,7 @@
 from vehicle import Vehicle
 
+# class Car is a subclass of Vehicle hence car inherits from Vehicle
 class Car(Vehicle):
-    pass
+    # overwriting the inherited methods from Vehicle
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,14 @@
 class Vehicle:
-    pass
+    
+    #def __init__ wheel_size and wheel_number are attributes 
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    # definition of the instance method go
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    # definition of the instance method fill_up_tank
+    def fill_up_tank(self):
+        return "filling up!"
